d89d47485a6da939c15c6ed00e53af16293b3e0a
Adam Topaz <github@adamtopaz.com>
2020-08-14 11:29:11 -0600

    More cleanup

--- src/to_mathlib.lean
+++ src/to_mathlib.lean
@@ -4,6 +4,11 @@ import data.fin
 namespace fin
 variables {α : Type*} {β : Type*}
 
+@[reducible]
+def inl {m n} : fin m → fin (m + n) := sum_fin_sum_equiv ∘ sum.inl  
+@[reducible]
+def inr {m n} : fin n → fin (m + n) := sum_fin_sum_equiv ∘ sum.inr  
+
 def of (a : α) : fin 1 → α := λ x, a
 def append {m n} (as : fin m → α) (bs : fin n → α) : fin (m + n) → α := 
   λ i, sum.cases_on (sum_fin_sum_equiv.inv_fun i) as bs
@@ -16,22 +21,31 @@ def compr {m n : ℕ} (as : fin (n + m) → α) (f : (fin m → α) → α) (g :
 def swap {m n : ℕ} : fin (m + n) → fin (n + m) :=
   λ i, fin.cast (nat.add_comm m n) i
 
+@[simp] lemma append_eval_inl {m n} (as : fin m → α) (bs : fin n → α) (i : fin m) : 
+  (append as bs) i.inl = as i := 
+begin
+  unfold append,
+  dsimp only [],
+  have : sum_fin_sum_equiv.inv_fun i.inl = sum.inl i, by simp,
+  rw this, 
+end
+
+@[simp] lemma append_eval_inr {m n} (as : fin m → α) (bs : fin n → α) (i : fin n) : 
+  (append as bs) i.inr = bs i := 
+begin
+  unfold append,
+  dsimp only [],
+  have : sum_fin_sum_equiv.inv_fun i.inr = sum.inr i, by simp,
+  rw this, 
+end
+
 @[simp] lemma map_append {m n} (as : fin m → α) (bs : fin n → α) (f : α → β) : 
   f ∘ (fin.append as bs) = fin.append (f ∘ as) (f ∘ bs) := 
 begin
-  sorry,
-  /-
-  ext,
-  by_cases x.val < m,
-  { let y : fin m := ⟨x.val, h⟩,
-    change f (as.append bs (inl y)) = (as.map f).append (bs.map f) (inl y),
-    repeat {rw append_eval_inl},
-    refl, },
-  { rw not_lt at h,
-    let y := fin.sub_nat m x.swap_args h,
-    repeat {rw eval_sub _ _  x h},
-    rw map_eval, }
-    -/
+  ext i,
+  have : i = sum_fin_sum_equiv (sum_fin_sum_equiv.symm i), by simp,
+  rw this, clear this,
+  cases sum_fin_sum_equiv.symm i; simp
 end
 
 @[simp] lemma swap_swap {m n : ℕ} (i : fin (m + n)) : swap (swap i) = i := by {ext, refl}
